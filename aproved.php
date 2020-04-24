c8378858385081df42164424e743897154ef879f
UlisesAguillon <2510362016@mail.utec.edu.sv>
2020-04-23 18:42:13 -0600

    formulacion

--- aproved.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo ' <div class="alert alert-success"><strong>Exito!</strong>Proyecto aprobado con  exito.</div>';
-          header( "refresh:3; url=/formulacion_de_presupuesto/index_admin2.php" );
- ?>
--- hecho.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo ' <div class="alert alert-success"><strong>Exito!</strong>Realizado exito.</div>';
-          header( "refresh:3; url=/formulacion_de_presupuesto/index.php" );
- ?>
